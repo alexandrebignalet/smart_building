6ee1d9d77027a0f81a336dd75ee9b9de3fb361ec
FCCAxl <pipajim64@gmail.com>
2017-12-11 13:47:37 +0100

    Adds value init for dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,18 +8,46 @@ export class DashboardComponent  {
 
     constructor() {
         console.log('Dashboard component const');
+        this.initValues();
     }
+
+    consommation: any;
+    prevision: any;
+
+    initValues() {
+        const facteurPrevision = 4;
+        this.lineChartData = [];
+        this.prevision = {
+            data: [],
+            label: 'Prevision'
+        };
+        this.consommation = {
+            data: [],
+            label: 'Consommation'
+        };
+
+        for (let i = 0 ; i < 7 ; i++) {
+            this.prevision.data.push(facteurPrevision - 2);
+            this.consommation.data.push(Math.round(facteurPrevision * (Math.random() * (2 - 1) + 0 ) * 100) / 100 );
+        }
+        this.lineChartData.push(this.prevision);
+        this.lineChartData.push(this.consommation);
+    }
+
     // lineChart
     // lineChart
     public lineChartData:Array<any> = [
-        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-        {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+
     ];
     public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     public lineChartOptions:any = {
         responsive: true
     };
+
+    public lineChartLegend:boolean = true;
+    public lineChartType:string = 'line';
+
+
     public lineChartColors:Array<any> = [
         { // grey
             backgroundColor: 'rgba(148,159,177,0.2)',
@@ -46,18 +74,10 @@ export class DashboardComponent  {
             pointHoverBorderColor: 'rgba(148,159,177,0.8)'
         }
     ];
-    public lineChartLegend:boolean = true;
-    public lineChartType:string = 'line';
+
 
     public randomize():void {
-        let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-        for (let i = 0; i < this.lineChartData.length; i++) {
-            _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-            for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-                _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-            }
-        }
-        this.lineChartData = _lineChartData;
+        this.initValues();
     }
 
     // events
@@ -68,4 +88,5 @@ export class DashboardComponent  {
     public chartHovered(e:any):void {
         console.log(e);
     }
+
 }
